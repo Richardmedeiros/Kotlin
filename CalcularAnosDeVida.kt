98c8015d7c60cee93bc7c0018129a7e011c77488
Richard S. Medeiros <126830459+Richardmedeiros@users.noreply.github.com>
2023-03-09 22:03:37 -0300

    Add files via upload

---
+++ CalcularAnosDeVida.kt
@@ -0,0 +1,17 @@
+/**
+ * Este responderá a pergunta abaixo.
+ * Como calcular quantos anos de vida uma pessoa tem?
+ */
+
+fun main () {
+
+    var anoDeNascimento = 2000
+    var anoAtual = 2021
+    var anosDeVida = anoAtual - anoDeNascimento
+
+    print ("Este programa calcula sua idade atual:\n")
+    print ("Data de Nascimento: $anoDeNascimento\n")
+    print ("Ano Atual.........: $anoAtual\n")
+    print ("Idade Atual.......: $anosDeVida Anos de vida.")
+
+}
