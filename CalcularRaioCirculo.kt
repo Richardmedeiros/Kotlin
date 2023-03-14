b80ce4402256063536ede9f2a887dd52a5020768
Richard S. Medeiros <126830459+Richardmedeiros@users.noreply.github.com>
2023-03-14 16:56:42 -0300

    Add files via upload

--- CalcularRaioCirculo.kt
+++ CalcularRaioCirculo.kt
@@ -1,9 +1,9 @@
-const val PI = 3.1415
+import kotlin.math.PI
 
 fun main () {
 
     var circunferencia = 15
-    var valorDoRaio = circunferencia / (2*PI)
+    var valorDoRaio = circunferencia / (2* PI)
 
     print ("Valor da circunferência: $circunferencia\n")
     print ("Valor do raio da circunferência: $valorDoRaio")
