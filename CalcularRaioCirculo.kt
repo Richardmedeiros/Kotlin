8432979f68fa8ae2d87776bffd076c66754a9b24
Richard S. Medeiros <126830459+Richardmedeiros@users.noreply.github.com>
2023-03-14 16:24:02 -0300

    Add files via upload

---
+++ CalcularRaioCirculo.kt
@@ -0,0 +1,10 @@
+const val PI = 3.1415
+
+fun main () {
+
+    var circunferencia = 15
+    var valorDoRaio = circunferencia / (2*PI)
+
+    print ("Valor da circunferência: $circunferencia\n")
+    print ("Valor do raio da circunferência: $valorDoRaio")
+}
